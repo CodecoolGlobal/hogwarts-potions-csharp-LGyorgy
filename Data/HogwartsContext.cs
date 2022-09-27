d04914a3060ce7d657b3d33604708342d9c607a4
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 09:36:07 +0200

    Update HogwartsContext with student and room entities

--- Data/HogwartsContext.cs
+++ Data/HogwartsContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HogwartsPotions.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace HogwartsPotions.Data;
 
@@ -6,5 +7,14 @@ public class HogwartsContext : DbContext
 {
     public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
     {
+    }
+
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>().ToTable("Student");
+        modelBuilder.Entity<Room>().ToTable("Room");
     }
 }
