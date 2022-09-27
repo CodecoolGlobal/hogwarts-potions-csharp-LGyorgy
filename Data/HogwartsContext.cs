495b4f9d883d9d99fd2f04cb44001d585f5f02e8
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 09:43:14 +0200

    Combine old HogwartsContext methods with the new one

--- Data/HogwartsContext.cs
+++ Data/HogwartsContext.cs
@@ -1,10 +1,15 @@
-﻿using HogwartsPotions.Models.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HogwartsPotions.Models.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace HogwartsPotions.Data;
 
 public class HogwartsContext : DbContext
 {
+    public const int MaxIngredientsForPotions = 5;
+
     public HogwartsContext(DbContextOptions<HogwartsContext> options) : base(options)
     {
     }
@@ -16,5 +21,35 @@ public class HogwartsContext : DbContext
     {
         modelBuilder.Entity<Student>().ToTable("Student");
         modelBuilder.Entity<Room>().ToTable("Room");
+    }
+
+    public async Task AddRoom(Room room)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Room> GetRoom(long roomId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Room>> GetAllRooms()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateRoom(Room room)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteRoom(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Room>> GetRoomsForRatOwners()
+    {
+        throw new NotImplementedException();
     }
 }
