35e50c468e461f36743bb84ae8185b4ae29f09c1
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 17:18:59 +0200

    Implement GetAllPotions

--- Services/PotionService.cs
+++ Services/PotionService.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using HogwartsPotions.Data;
 using HogwartsPotions.Models.Entities;
 using HogwartsPotions.Services.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace HogwartsPotions.Services;
 
 public class PotionService : IPotionService
 {
+    private HogwartsContext _context;
+
+    public PotionService(HogwartsContext context)
+    {
+        _context = context;
+    }
+
     public Task<List<Potion>> GetAllPotions()
     {
-        throw new System.NotImplementedException();
+        return _context.Potions
+            .Include(p => p.Brewer)
+                .ThenInclude(s => s.Room)
+            .Include(p => p.Ingredients)
+            .Include(p => p.Recipe)
+                .ThenInclude(p => p.Ingredients)
+            .ToListAsync();
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using HogwartsPotions.Data;
 using HogwartsPotions.Services;
 using HogwartsPotions.Services.Interface;
@@ -28,7 +29,12 @@ namespace HogwartsPotions
             services.AddScoped<IRoomService, RoomService>();
             services.AddScoped<IPotionService, PotionService>();
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
