e5622fd3b684783580f5d92407361a12a914c402
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 16:27:53 +0200

    Create PotionService

---
+++ Services/PotionService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using HogwartsPotions.Models.Entities;
+using HogwartsPotions.Services.Interface;
+
+namespace HogwartsPotions.Services;
+
+public class PotionService : IPotionService
+{
+    public List<Potion> GetAllPotions()
+    {
+        throw new System.NotImplementedException();
+    }
+}
