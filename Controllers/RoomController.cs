9d5abd69ccf1ecea735a4f131a236df2c3df1745
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 10:42:10 +0200

    Fix routes

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -28,25 +28,25 @@ namespace HogwartsPotions.Controllers
             await _context.AddRoom(room);
         }
 
-        [HttpGet("/{id}")]
+        [HttpGet("{id}")]
         public async Task<Room> GetRoomById(long id)
         {
             return await _context.GetRoom(id);
         }
 
-        [HttpPut("/{id}")]
+        [HttpPut("{id}")]
         public void UpdateRoomById(long id, [FromBody] Room updatedRoom)
         {
             _context.Update(updatedRoom);
         }
 
-        [HttpDelete("/{id}")]
+        [HttpDelete("{id}")]
         public async Task DeleteRoomById(long id)
         {
             await _context.DeleteRoom(id);
         }
 
-        [HttpGet("/rat-owners")]
+        [HttpGet("rat-owners")]
         public async Task<List<Room>> GetRoomsForRatOwners()
         {
             return await _context.GetRoomsForRatOwners();
