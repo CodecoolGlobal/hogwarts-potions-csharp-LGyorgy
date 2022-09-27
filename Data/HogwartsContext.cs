ced99f30549987d1818ba06d67807c79195aa2a2
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 16:13:50 +0200

    Add Potion to DbContext

--- Data/HogwartsContext.cs
+++ Data/HogwartsContext.cs
@@ -20,6 +20,7 @@ public class HogwartsContext : DbContext
     public DbSet<Room> Rooms { get; set; }
     public DbSet<Recipe> Recipes { get; set; }
     public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<Potion> Potions { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -34,5 +35,8 @@ public class HogwartsContext : DbContext
 
         modelBuilder.Entity<Ingredient>()
             .ToTable("Ingredient");
+
+        modelBuilder.Entity<Ingredient>()
+            .ToTable("Potion");
     }
 }
