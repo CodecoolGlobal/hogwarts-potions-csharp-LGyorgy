4835fad04dda16db7797228a3f3618fd9aa627fc
LGyorgy <l.gyorgy90@gmail.com>
2022-09-27 16:13:03 +0200

    Create Potion entity class

---
+++ Models/Entities/Potion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using HogwartsPotions.Models.Enums;
+
+namespace HogwartsPotions.Models.Entities;
+
+public class Potion
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long ID { get; set; }
+    public string Name { get; set; }
+    public Student Brewer { get; set; }
+    public List<Ingredient> Ingredients { get; set; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public BrewingStatus BrewingStatus { get; set; }
+}
