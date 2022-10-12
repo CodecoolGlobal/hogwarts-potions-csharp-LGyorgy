6a2ef95ead490de2d5934d01f703acbb53c58e2a
LGyorgy <l.gyorgy90@gmail.com>
2022-10-12 15:50:25 +0200

    Implement PotionCreator component

--- HogwartsPotions-Frontend/src/components/potionCreator.jsx
+++ HogwartsPotions-Frontend/src/components/potionCreator.jsx
@@ -1,7 +1,40 @@
-const PotionCreator = () => {
+import { useState } from "react";
+
+const PotionCreator = ({ handleNewPotion }) => {
+    const [value, setValue] = useState("");
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const studentId = value;
+        const url = "potions/brew";
+        const options = {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({ studentId })
+        };
+        const potion = await fetch(url, options)
+            .then(r => r.json());
+        handleNewPotion(potion)
+    }
+
     return (
         <>
-            <div>Create new potion page</div>
+            <h2>Create new potion</h2>
+            <form onSubmit={onSubmit}>
+                <ul>
+                    <li>
+                        <label htmlFor="studentId">Student ID: </label>
+                        <input value={value} onChange={onChange} type="number"></input>
+                    </li>
+                </ul>
+                <button type="submit">Start brewing a potion</button>
+            </form>
         </>
     );
 }
