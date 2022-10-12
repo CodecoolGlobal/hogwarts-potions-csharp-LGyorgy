511664d277aa2c7f17d2b70a31a78761d8239108
LGyorgy <l.gyorgy90@gmail.com>
2022-10-12 15:50:54 +0200

    Create IngredientAdder component

--- HogwartsPotions-Frontend/src/components/ingredientAdder.jsx
+++ HogwartsPotions-Frontend/src/components/ingredientAdder.jsx
@@ -1,7 +1,41 @@
-const IngredientAdder = ({ potion }) => {
+import { useState } from "react";
+
+const IngredientAdder = ({ potion, handleNewPotion }) => {
+    const [value, setValue] = useState("");
+
+    const onChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const name = value;
+        const url = `potions/${potion.id}/add`;
+        const options = {
+            method: "PUT",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({ name })
+        };
+        const newPotion = await fetch(url, options)
+            .then(r => r.json());
+        handleNewPotion(newPotion);
+        setValue("");
+    }
+
     return (
         <>
-            <div>Add ingredient page</div>
+            <h2>Add ingredient</h2>
+            <form onSubmit={onSubmit}>
+                <ul>
+                    <li>
+                        <label htmlFor="ingredient">Ingredient: </label>
+                        <input value={value} onChange={onChange} type="text"></input>
+                    </li>
+                </ul>
+                <button type="submit">Add ingredient</button>
+            </form>
         </>
     );
 }
