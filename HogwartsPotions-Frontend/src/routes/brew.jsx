be85bb0c59dc261effa18f75e1961f66aa6d1c22
LGyorgy <l.gyorgy90@gmail.com>
2022-10-12 20:35:35 +0200

    Update brew page style

--- HogwartsPotions-Frontend/src/routes/brew.jsx
+++ HogwartsPotions-Frontend/src/routes/brew.jsx
@@ -1,7 +1,9 @@
 import {
-    Form,
+    Form as RouterForm,
     redirect,
-  } from "react-router-dom";
+} from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { brewPotion } from "../services/potionService";
 
 const action = async ({ request }) => {
@@ -17,16 +19,18 @@ const Brew = () => {
 
     return (
         <>
-            <h2>Create new potion</h2>
-            <Form method="post">
-                <ul>
-                    <li>
-                        <label htmlFor="studentId">Student ID: </label>
-                        <input name="studentId" type="number"></input>
-                    </li>
-                </ul>
-                <button type="submit">Start brewing a potion</button>
-            </Form>
+            <div className="card mx-auto mt-5 p-3" style={{"width": "50%"}}>
+                <h3 className="text-center card-title">Create new potion</h3>
+                <RouterForm method="post">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Student ID:</Form.Label>
+                        <Form.Control name="studentId" type="number" placeholder="Enter student id" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Start brewing a potion
+                    </Button>
+                </RouterForm>
+            </div>
         </>
     );
 }
