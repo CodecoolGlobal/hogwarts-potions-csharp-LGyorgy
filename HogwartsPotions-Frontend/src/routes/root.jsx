79fb55aa3f81cf9a14e34467be598d0c9d56f331
LGyorgy <l.gyorgy90@gmail.com>
2022-10-12 22:08:08 +0200

    Update navbar color

--- HogwartsPotions-Frontend/src/routes/root.jsx
+++ HogwartsPotions-Frontend/src/routes/root.jsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const Root = () => {
     return <>
-        <Navbar bg="light">
+        <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand>Hogwarts Potions</Navbar.Brand>
             </Container>
