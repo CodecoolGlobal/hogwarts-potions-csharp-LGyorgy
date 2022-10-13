a2b51c4550fcd6e6c50807b157247347f1a13525
LGyorgy <l.gyorgy90@gmail.com>
2022-10-13 11:21:41 +0200

    Add messege for empty recipe list

--- HogwartsPotions-Frontend/src/components/potionHelper.jsx
+++ HogwartsPotions-Frontend/src/components/potionHelper.jsx
@@ -1,13 +1,18 @@
 import RecipeCard from "./recipeCard";
 
 const PotionHelper = ({ recipes }) => {
+    if (recipes.length === 0) {
+        return <div className="text-center">
+            No compatible recipes have been discovered yet...
+        </div>
+    }
+
     return (
         recipes.map((recipe) =>
             <div className="my-3">
                 <RecipeCard recipe={recipe} />
             </div>
         )
-
     );
 }
 
